d12a4def6fbe1a4b2ccdb9089474821baa53c506
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2024-12-29 12:41:41 +0000

    Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@nextui-org/button";
+
 export default function Home() {
   return (
-    <h1>Personal Database</h1>
+    <div>
+      <h1 className="mt-5 text-6xl">Personal Database</h1>
+      <p className="mt-5 text-xl">A personal database for all your needs</p>
+      <Button color="primary" radius="sm" className="mt-5">
+        Get Started
+      </Button>
+    </div>
   );
 }
